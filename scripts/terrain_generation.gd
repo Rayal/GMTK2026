1a526c60f8549487d2e7987bfedfdaddee0833ea
TaaviVaikjarv <taaviv@gmail.com>
2026-07-25 00:21:08 +0300

    New terrain generation concept

--- scripts/terrain_generation.gd
+++ scripts/terrain_generation.gd
@@ -5,37 +5,34 @@ signal terrain_limits(top_left: Vector2, bottom_right: Vector2)
 var rng = RandomNumberGenerator.new()
 
 @export var noise = FastNoiseLite.new()
-@export var width: int = 30
-@export var height: int = 30
+@export var width: int = 50
+@export var height: int = 50
 @export var noise_scale: float = 0.1 
-@export var water_height: float = -0.1
-@export var plains_height: float = 0
-@export var hill_height: float = 0.1
+@export var forest_threshold: float = -0.1
 
-@export var terrain: TileMapLayer 
+@export var terrain: TileMapLayer
+@export var terrain_objects: TileMapLayer
 
 func _ready() -> void:
-	create_terrain()
+	create_base_terrain()
 	emit_limit()
+	create_objects_on_terrain()
+
 
 func generate_seed():
 	noise.seed = rng.randi()
 
-func create_terrain():
+func create_base_terrain():
 	generate_seed()
 	terrain.clear()
 	for x in range(width):
 		for y in range(height):
 			var n2d = noise.get_noise_2d(x * noise_scale, y * noise_scale)
-			if n2d < water_height:
-				terrain.set_cell(Vector2i(x,y), 2, Vector2i(8, 11))
-			elif n2d < plains_height and n2d >= water_height:
-				terrain.set_cell(Vector2i(x,y), 2, Vector2i(1, 1))
-			else:
-				terrain.set_cell(Vector2i(x,y), 2, Vector2i(0, 5))
-				
-			terrain.get_cell_tile_data(Vector2i(x,y)).set_custom_data("height", n2d)
-			
+			terrain.set_cell(Vector2i(x,y), 0, Vector2i(0, 0))
+			if n2d < forest_threshold:
+				terrain_objects.set_cell(Vector2i(x,y), 0, Vector2i(8, 7))
+
+
 func emit_limit():
 	var rect = terrain.get_used_rect()
 	var cell_size = terrain.tile_set.tile_size
@@ -44,3 +41,14 @@ func emit_limit():
 	print(top_left)
 	print(bottom_right)
 	terrain_limits.emit(top_left, bottom_right)
+
+
+
+func create_objects_on_terrain():
+	
+	for i in range(5):
+		var terrain_feature = terrain_objects.get_children().pick_random()
+		terrain_feature.visible = true
+		terrain_feature.rotate(deg_to_rad(90))
+		terrain_feature.position.x = randi() % width * 10
+		terrain_feature.position.y = randi() % height * 10
