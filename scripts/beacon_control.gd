c29f2cc36abaada5ec613a415b8c871a39f4638d
Andreas Ragen Ayal <5462920+Rayal@users.noreply.github.com>
2026-07-25 21:21:04 +0300

    Finally solved the overlapping triangles issue. Claude helped with the coding.

--- scripts/beacon_control.gd
+++ scripts/beacon_control.gd
@@ -4,7 +4,7 @@ signal new_beacon(beacon: Beacon)
 
 @export var beacon_number: int = 0
 @export var triangle_number: int = 0
-@export var area_mapped: float = 0.0
+@export var area_mapped: int = 0
 
 @export var beacon_asset_path: String
 @export var triangle_asset_path: String
@@ -18,7 +18,7 @@ var triangles: Dictionary[int, Triangle] = {}
 func _ready() -> void:
 	beacon_resource = load(beacon_asset_path)
 	triangle_resource = load(triangle_asset_path)
-	update_label()
+	update_labels()
 
 
 func _on_new_beacon_request() -> void:
@@ -27,29 +27,57 @@ func _on_new_beacon_request() -> void:
 	beacons[beacon.id] = beacon
 	new_beacon.emit(beacon)
 	beacon_number += 1
-	update_label()
+	update_labels()
 
 
 func _on_triangle_request(beacon: Beacon, beacons: Array[Beacon]):
+	if beacons.size() == 2:
+		beacons.append(beacon)
+		create_triangle(beacons)
+		return
+
 	beacons.sort_custom(
-		func(a,b):
+		func(a, b):
 			return (
-						beacon.position.angle_to_point(a.position) <=
-						beacon.position.angle_to_point(b.position)
+						beacon.global_position.angle_to_point(a.global_position) <=
+						beacon.global_position.angle_to_point(b.global_position)
 					)
 	)
-	for i in range(beacons.size() - 1):
-		create_triangle([beacon, beacons[i], beacons[i+1]])
+
+	var n: int = beacons.size()
+	var raw_angles: Array[float] = []
+	for b in beacons:
+		raw_angles.append(beacon.global_position.angle_to_point(b.global_position))
+
+	var gaps: Array[float] = []
+	for i in range(n - 1):
+		gaps.append(raw_angles[i + 1] - raw_angles[i])
+	gaps.append(TAU - (raw_angles[n - 1] - raw_angles[0]))
+
+	var max_gap_index: int = 0
+	for i in range(1, n):
+		if gaps[i] > gaps[max_gap_index]:
+			max_gap_index = i
+
+	var ordered: Array[Beacon] = []
+	for i in range(n):
+		ordered.append(beacons[(max_gap_index + 1 + i) % n])
+
+	for i in range(n - 1):
+		create_triangle([beacon, ordered[i], ordered[i + 1]])
+
 
 func create_triangle(beacons: Array[Beacon]):
 	var triangle = triangle_resource.instantiate()
 	triangle.vertices = beacons
 	triangle.id = triangle_number
 	triangle.init()
+	area_mapped += triangle.area
 	triangles[triangle.id] = triangle
 	get_tree().current_scene.add_child(triangle)	
 	triangle_number += 1
 
 
-func update_label():
-	$Label.text = "Beacons Placed: " + str(beacon_number)
+func update_labels():
+	$BeaconLabel.text = "Beacons Placed: " + str(beacon_number)
+	$TriangleLabel.text = "\nArea Mapped: " + str(area_mapped)
--- scripts/triangle.gd
+++ scripts/triangle.gd
@@ -2,7 +2,10 @@ class_name Triangle extends Area2D
 
 @export var id: int
 @export var vertices: Array[Beacon]
-@export var area: float
+@export var area: int
+
+@export var colour: Color = Color.DARK_ORCHID
+@export_range(0.0, 1.0) var fill_alpha: float = 0.35
 
 var vertices2D: PackedVector2Array = []
 
@@ -10,7 +13,19 @@ func init() -> void:
 	for vertex: Beacon in vertices:
 		vertices2D.append(vertex.global_position)
 	$CollisionPolygon2D.polygon = vertices2D
-	
+	area = (
+		0.5 * 
+		vertices[0].global_position.distance_to(vertices[1].global_position) *
+		vertices[0].global_position.distance_to(vertices[2].global_position) *
+		abs(sin(
+			vertices[0].global_position.angle_to_point(vertices[1].global_position) - 
+			vertices[0].global_position.angle_to_point(vertices[2].global_position)
+		)) / (32 * 32 * 5 * 5)
+	)
 
 func _draw() -> void:
-	draw_colored_polygon(vertices2D, Color.DARK_ORCHID)
+	draw_line(vertices2D[0], vertices2D[1], colour, 2)
+	draw_line(vertices2D[0], vertices2D[2], colour, 2)
+	draw_line(vertices2D[2], vertices2D[1], colour, 2)
+	
+	draw_colored_polygon(vertices2D, Color(colour, fill_alpha))
