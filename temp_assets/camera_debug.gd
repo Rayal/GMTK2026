6445b1fb0bc66ed6c99d9b78565ff65f536f1b9f
Andreas Ragen Ayal <5462920+Rayal@users.noreply.github.com>
2026-07-26 19:38:50 +0300

    Removed temp assets

--- temp_assets/camera_debug.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-func _process(delta: float) -> void:
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction:
-		position += direction * 2000 * delta
-	
