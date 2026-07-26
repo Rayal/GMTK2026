e84066f55a6d5963df7e3b91e04fa11e045ea903
TaaviVaikjarv <taaviv@gmail.com>
2026-07-26 16:56:37 +0300

    Background for menus

--- settings.gd
+++ settings.gd
@@ -1,8 +1,8 @@
 extends Node
 
 
-var map_width: int = 50
-var map_height: int = 50
+var map_width: int = 300
+var map_height: int = 300
 var noise_scale: float = 0.1 
 var forest_threshold: float = -0.1
 
